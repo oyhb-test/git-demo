973ab1d402455126415905230a233a71a3822ad7
zhangsan <lisi>
2020-03-18 00:24:27 +0800

    项目经理第一次提交 git_test

---
+++ src/main/java/com/oyhb/git_test/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.oyhb.git_test.controller;/*
+@author hbouyang
+@date 2020/3/18 - 0:12
+*/
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello/{msg}")
+    public Object hello(@PathVariable("msg") String msg){
+        return msg;
+    }
+
+}
